5606e522467b1d26c60f95bdf5be675863ad1766
Pawel Skalski <skalskipawel78@gmail.com>
2019-09-08 19:48:24 +0200

    Second try

---
+++ rename.py
@@ -0,0 +1,19 @@
+import os
+#
+os.chdir("f:\Music\The Pogues - If I Should Fall From Grace With God")
+print(os.getcwd())
+
+# for f in os.listdir():
+#     file_name, file_ext = os.path.splitext(f)
+#     if file_ext =='.mpc':
+#         band_title, album_title, s_num, song_title = file_name.split('-')
+#         # s_num = s_num.strip().zfill(3)
+#         new_name = '{}- {}{}{}'.format(s_num, band_title, song_title.strip(), file_ext)
+#         print(new_name)
+#         # os.rename(f, new_name)
+
+
+
+
+
+
