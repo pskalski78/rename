bc9dbefaac02fdc90ef75cd8e8c1ab12c614608c
Pawel Skalski <skalskipawel78@gmail.com>
2019-09-08 20:02:47 +0200

    Add some functions

--- rename.py
+++ rename.py
@@ -13,6 +13,20 @@ print(os.getcwd())
 #         # os.rename(f, new_name)
 
 
+def add(x,y):
+    pass
+
+def subtract(x,y):
+    pass
+
+def multiply(x,y):
+    return x*y
+
+def divide(x,y):
+    pass
+
+def squere(x,y):
+    pass
 
 
 
