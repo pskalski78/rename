10e5bb60e02af708b45f10db9668860fb2201653
Pawel Skalski <skalskipawel78@gmail.com>
2019-09-10 23:26:00 +0200

    multiply function

--- rename.py
+++ rename.py
@@ -1,20 +1,5 @@
-import os
-#
-os.chdir("f:\Music\The Pogues - If I Should Fall From Grace With God")
-print(os.getcwd())
-
-# for f in os.listdir():
-#     file_name, file_ext = os.path.splitext(f)
-#     if file_ext =='.mpc':
-#         band_title, album_title, s_num, song_title = file_name.split('-')
-#         # s_num = s_num.strip().zfill(3)
-#         new_name = '{}- {}{}{}'.format(s_num, band_title, song_title.strip(), file_ext)
-#         print(new_name)
-#         # os.rename(f, new_name)
-
-
 def add(x,y):
-    pass
+    return x + y
 
 def subtract(x,y):
     return x + y
